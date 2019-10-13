e2043df48661b2a614a035368cf7badb83b7716e
Eduardo Júnior <ed.afram@gmail.com>
2019-10-13 15:04:14 -0300

    Correção na resposta do dado na tela.

--- client.py
+++ client.py
@@ -3,11 +3,14 @@ import socket
 host = '127.0.0.1'
 port = 12345
 
-numero = ''
-while not numero.isdecimal():
-    numero = input('Digite um número: ')
+number = ''
+while not number.isdecimal():
+    number = input('Digite um número: ')
 try:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((host, port))
-    client.send(numero.encode())
-    response = client.recv(1024)
+    client.send(number.encode())
+    result = float(client.recv(1024))
+    print('25% de {0} é {1:2}'.format(number, result))
+except:
+    print('Um erro ocorreu ao tentar conectar-se')
