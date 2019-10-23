6b52f3ece89d22bc3d210a99371c7611ba6e8ad3
Eduardo Júnior <ed.afram@gmail.com>
2019-10-23 20:19:04 -0300

    Implementação funcionando.

--- UDPClient.py
+++ UDPClient.py
@@ -1,17 +1,17 @@
 import socket
 
-host = '0.0.0.0'
-port = 12000
+host = '127.0.0.1'
+port = 12345
 
 number = ''
-while not number.isdecimal:
-  number = input('Digite um número: ')
-
+while not number.isdecimal():
+    number = input('Digite um número: ')
 try:
-  client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  client.sendto(number, (host, port))
-  result, addr = client.recvfrom(2048)
-  client.close()
-  print('25% de {0} é {1:2}'.format(number, result))
-except:
-  print('Um erro ocorreu ao tentar conectar-se')
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client.sendto(number.encode(), (host, port))
+    data, addr = client.recvfrom(1024)
+    result = float(data.decode())
+    print('25% de {0} é {1:2}'.format(number, result))
+    client.close()
+except Exception as e:
+    print('Um erro ocorreu ao durante a transmissão.')
--- UDPServer.py
+++ UDPServer.py
@@ -1,18 +1,17 @@
 import socket
 
-host = ''
-port = 12000
+host = '127.0.0.1'
+port = 12345
 
-try:
-  server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  server.bind((host, port))
-  print('Servidor escutando...')
-  while True:
-    data, addr = server.recvfrom(2048)
-    if str(data).isdecimal():
-      response = str(float(data) * 0.25)
-      server.sendto(response, addr)
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind((host, port))
+print('Servidor escutando...')
+while True:
+    data, addr = server.recvfrom(1024)
+    value = data.decode()
+    if value.isdecimal():
+        result = str(float(value) * 0.25)
+        server.sendto(result.encode(), addr)
+        print('{} enviado para {}'.format(result, addr))
     else:
-      print('Dado em formato não decimal.. =/')
-except:
-  print('ocorreu um erro')
+        print('Dado em formato não decimal.. =/')
