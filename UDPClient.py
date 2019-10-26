7151cf9dd3f63c0506873131d79bb455890e1145
Flakes <30809620+snowflakesss@users.noreply.github.com>
2019-10-26 12:07:39 -0300

    Inclusão de comentários explicativos de cada linha.

--- UDPClient.py
+++ UDPClient.py
@@ -3,15 +3,25 @@ import socket
 host = '127.0.0.1'
 port = 12345
 
+# declarando a variável de tipo string ↓
 number = ''
+# repetirá enquanto o número digitado não for decimal ↓
 while not number.isdecimal():
+    # lendo o número digitado pelo usuário ↓
     number = input('Digite um número: ')
 try:
+    # instanciando o socket UDP para transmissão de dados em stream ↓
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # enviando o número digitado pelo usuário para o servidor ↓
     client.sendto(number.encode(), (host, port))
+    # recebendo a resposta do servidor ↓
     data, addr = client.recvfrom(1024)
+    # convertendo o dado para float ↓
     result = float(data.decode())
+    # printando na tela o resultado enviado pelo servidor ↓
     print('25% de {0} é {1:2}'.format(number, result))
+    # fechando o socket cliente ↓
     client.close()
-except Exception as e:
-    print('Um erro ocorreu ao durante a transmissão.')
+except:
+    # alertando que ocorreu um erro ↓
+    print('Ocorreu um erro durante a transmissão.')
--- UDPServer.py
+++ UDPServer.py
@@ -3,15 +3,25 @@ import socket
 host = '127.0.0.1'
 port = 12345
 
+# instanciando o socket UDP para transmissão de dados em stream ↓
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# ligando o socket a um endereço através do host e porta ↓
 server.bind((host, port))
+# informando que o servidor está disponível para conexão↓
 print('Servidor escutando...')
 while True:
+    # recebendo o dado e o endereço enviado pelo cliente ↓
     data, addr = server.recvfrom(1024)
+    # transformando o dado de array de bytes para string ↓
     value = data.decode()
+    # verificando se o dado enviado é um número de ponto flutuante ↓
     if value.isdecimal():
+        # calculando 25% desse valor e transformando em string ↓
         result = str(float(value) * 0.25)
+        # enviando o valor calculado para o cliente ↓
         server.sendto(result.encode(), addr)
+        # informando o resultado e para qual endereço foi enviado ↓
         print('{} enviado para {}'.format(result, addr))
     else:
+        # mensagem de erro caso o dado enviado não seja string decimal ↓
         print('Dado em formato não decimal.. =/')
